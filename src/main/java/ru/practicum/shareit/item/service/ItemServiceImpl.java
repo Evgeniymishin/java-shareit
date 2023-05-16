1389c4f6eb33528dc5112fa35311e9bdc12ec84d
Evgeniy Mishin <dubreak@yandex.ru>
2023-05-16 22:32:07 +0300

    feat: Функционал Item

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.storage.ItemStorage;
+import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.storage.UserStorage;
+import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +19,13 @@ import static ru.practicum.shareit.item.ItemMapper.toItemDto;
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ItemServiceImpl implements ItemService {
-    private final ItemStorage itemStorage;
-    private final UserStorage userStorage;
+    private final ItemRepository itemRepository;
+    private final UserRepository userRepository;
 
     @Override
     public List<ItemDto> getAll(Long userId) {
         List<ItemDto> items = new ArrayList<>();
-        for (Item item : itemStorage.getAll()) {
+        for (Item item : itemRepository.findAll()) {
             if (item.getOwner().getId().equals(userId)) {
                 items.add(toItemDto(item));
             }
@@ -35,7 +35,7 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public ItemDto getById(Long id) {
-        Item item = itemStorage.getById(id)
+        Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Не найдена вещь с id: " + id));
 
         return toItemDto(item);
@@ -43,18 +43,18 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public ItemDto create(ItemDto itemDto, Long userId) {
-        User user = userStorage.getById(userId)
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new NotFoundException("Ошибка создания вещи - " +
                         "не найден пользователь с id: " + userId));
         Item item = toItem(itemDto);
         item.setOwner(user);
-        itemStorage.create(item);
+        itemRepository.save(item);
         return toItemDto(item);
     }
 
     @Override
     public ItemDto update(ItemDto itemDto, Long id, Long userId) {
-        Item item = itemStorage.getById(id)
+        Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Не найдена вещь с id: " + id));
         if (!item.getOwner().getId().equals(userId)) {
             throw new NotFoundException("Ошибка обновления вещи: у пользователя " + userId + "нет такой вещи");
@@ -68,13 +68,13 @@ public class ItemServiceImpl implements ItemService {
         if (itemDto.getAvailable() != null) {
             item.setAvailable(itemDto.getAvailable());
         }
-        return toItemDto(itemStorage.update(item));
+        return toItemDto(itemRepository.save(item));
     }
 
     @Override
     public void delete(Long id) {
         getById(id);
-        itemStorage.delete(id);
+        itemRepository.deleteById(id);
     }
 
     @Override
@@ -83,7 +83,7 @@ public class ItemServiceImpl implements ItemService {
         if (text.isBlank()) {
             return searchedItems;
         }
-        for (Item item : itemStorage.getAll()) {
+        for (Item item : itemRepository.findAll()) {
             if (item.getName().toLowerCase().contains(text.toLowerCase()) ||
                     item.getDescription().toLowerCase().contains(text.toLowerCase()) && item.getAvailable()) {
                 searchedItems.add(toItemDto(item));
